12b1d169d01d005c307f432543da249ca8171c2d
Sktanisha <sanjidatanisha99@gmail.com>
2026-06-17 13:44:19 -0400

    1st commit

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import { Jost } from "next/font/google";
+
+const jost = Jost({
+  subsets:["latin"],
+  variable:"--font-jost",
+});
+export default function RootLayout({children}) {
+  return (
+    <html
+      lang="en"
+    >
+      <body>
+        className={jost.variable}
+        {children}</body>
+    </html>
+  );
+}
