bad45d3465a20d164fb0ec6c9984bb92bf287aed
Sktanisha <sanjidatanisha99@gmail.com>
2026-07-16 01:03:37 -0400

    ..

--- components/shop/ShopBanner.jsx
+++ components/shop/ShopBanner.jsx
@@ -3,7 +3,7 @@ import ShopBannerBgImage from "@/assets/images/shopBannerbgImage.png";
 const ShopBanner = () => {
   return (
     <div
-      className=" pt-37.5 pb-50.75 mx-15 text-center bg-no-repeat! bg-cover! bg-center!"
+      className=" pt-35 pb-20 mx-15 text-center bg-no-repeat! bg-cover! bg-center!"
       style={{ background: `url(${ShopBannerBgImage.src})` }}
     >
       <h2 className="text-[60px] font-bold text-primary-black uppercase">
--- components/shop/SortAndView.jsx
+++ components/shop/SortAndView.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react'
+import React from "react";
 import {
   Select,
   SelectContent,
@@ -8,58 +8,68 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import {useProductView} from '../../store/useProductView';
+import { useProductView } from "../../store/useProductView";
 const SortAndView = () => {
-  const {setView}= useProductView();
-  const handleView = (view)=>{
+  const { view, setView } = useProductView();
+  const handleView = (view) => {
     setView(view);
-  }
+  };
   const items = [
     { label: "T-shirt", value: "T-shirt" },
     { label: "Sharee", value: "Sharee" },
     { label: "Punjabi", value: "Punjabi" },
   ];
-  const View =[2,3,4];
+  const View = [2, 3, 4];
   return (
-    
-    <div className='flex items-center gap-10'>
+    <div className="flex items-center gap-10">
       <div>
         <Select>
-                  <SelectTrigger className="w-full border-none font-medium text-primary-black uppercase ">
-                    <SelectValue placeholder="Default sorting" />
-                  </SelectTrigger>
-        
-                  <SelectContent>
-                    <SelectGroup>
-                      {items.map((item) => (
-                        <SelectItem key={item.value} value={item.value}>
-                          {item.label}
-                        </SelectItem>
-                      ))}
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
+          <SelectTrigger className="w-56 border-none font-medium text-primary-black uppercase ">
+            <SelectValue placeholder="Default sorting" />
+          </SelectTrigger>
+
+          <SelectContent>
+            <SelectGroup>
+              {items.map((item) => (
+                <SelectItem key={item.value} value={item.value}>
+                  {item.label}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
       <div>
-        <ul className='flex items-center gap-4 '>
+        <ul className="flex items-center gap-4 ">
           <li>
             <button>VIEW</button>
           </li>
-          
-            {
-              View.map((item)=>(
-                <li key={item}>
-                  <button className='cursor-pointer' onClick={()=>handleView(item)}>{item}</button>
-                </li>
-              ))
-            }
-            
-          
-          
+
+          {View.map((item) =>
+            view === item ? (
+              <li key={item}>
+                <button
+                  className="cursor-pointer bg-primary-black text-white px-2 py-1 rounded-md"
+                  onClick={() => handleView(item)}
+                >
+                  {item}
+                </button>
+              </li>
+            ) : (
+              <li key={item}>
+                <button
+                  className="cursor-pointer "
+                  onClick={() => handleView(item)}
+                >
+                  {item}
+                </button>
+              </li>
+            ),
+          )}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SortAndView
+export default SortAndView;
