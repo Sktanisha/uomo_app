a5c37bd1c5c51524b484b95742696c8816900e62
Sktanisha <sanjidatanisha99@gmail.com>
2026-07-21 00:19:52 -0400

    searchinput done

--- components/layout/header/Searchinput.jsx
+++ components/layout/header/Searchinput.jsx
@@ -8,24 +8,47 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Search } from "lucide-react";
-
-const Searchinput = () => {
+import { useState } from "react";
+import { useProduct } from "../../../store/useProduct";
+import { useRouter } from "next/navigation";
+const Searchinput = ({ className }) => {
+  const router = useRouter();
+  const [searchProducts, setSearchProducts] = useState([]);
+  const { product } = useProduct();
   const items = [
     { label: "T-shirt", value: "T-shirt" },
     { label: "Sharee", value: "Sharee" },
     { label: "Punjabi", value: "Punjabi" },
   ];
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    /* let searchProduct = products.filter((product)=> 
+      product.title.toLowerCase().includes(e.target.value.toLowerCase()),
+  ); */
+    const searchProduct = product.filter((item) =>
+      item.title.toLowerCase().includes(e.target.value.toLowerCase()),
+    );
+    if (!value.trim()) {
+      setSearchProducts([]);
+      return;
+    }
 
+    const filtered = product.filter((item) =>
+      item.title.toLowerCase().includes(value.toLowerCase()),
+    );
+
+    setSearchProducts(filtered);
+  };
   return (
-    <div className="relative flex h-12 w-full items-center">
+    <div className={`relative flex h-12 w-full items-center ${className}`}>
       <input
+        onChange={handleSearch}
         type="text"
         placeholder="Search products..."
         className="h-full w-full rounded-sm bg-white pl-4 pr-36 text-sm outline-none"
       />
 
       <div className="absolute right-3 flex items-center gap-3">
-
         <Select>
           <SelectTrigger className="w-full border-none shadow-none">
             <SelectValue placeholder="All Category" />
@@ -45,8 +68,30 @@ const Searchinput = () => {
         <button>
           <Search size={20} />
         </button>
-
       </div>
+
+      {searchProducts.length > 0 && (
+  <div className="absolute top-[52px] left-0 z-50 max-h-80 w-full overflow-y-auto rounded-md border bg-white shadow-lg">
+
+    {searchProducts.map((item) => (
+      <div
+        key={item.id}
+        onClick={() => {
+          router.push(`/shop/${item.id}`);
+          setSearchProducts([]);
+        }}
+        className="cursor-pointer border-b px-4 py-3 transition hover:bg-gray-100"
+      >
+        <p className="font-medium">{item.title}</p>
+
+        <p className="text-sm text-gray-500">
+          ${item.price}
+        </p>
+      </div>
+    ))}
+
+  </div>
+)}
     </div>
   );
 };
